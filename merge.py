315ffc2a3fcb0e35ffa9e3fff15bfe08e13c6dfc
Alex Morega <alex@grep.ro>
2014-02-23 16:20:19 +0200

    script to mege data from spreadsheet

---
+++ merge.py
@@ -0,0 +1,37 @@
+import csv
+import requests
+from StringIO import StringIO
+import simplejson as json
+from path import path
+
+
+SPREADSHEET_CSV_URL_TEMPLATE = (
+    'https://docs.google.com/spreadsheet/pub'
+    '?key={key}&single=true&gid=0&output=csv'
+)
+
+def create_csv_url(key):
+    return SPREADSHEET_CSV_URL_TEMPLATE.format(key=key)
+
+
+def get_gdrive_csv(key):
+    url = create_csv_url(key)
+    resp = requests.get(url)
+    assert resp.headers['Content-Type'] == 'text/csv'
+    text = resp.content
+    return csv.DictReader(StringIO(text))
+
+
+new_data = {}
+for row in get_gdrive_csv('0ApN4Tp2ea9EKdG8yWHB2VWxieGlTMkM4Nm1ENzhVc2c'):
+    if row['id']:
+        new_data[int(row['id'])] = row
+
+p = path('biblioteci.geojson')
+data = json.loads(p.text())
+for feature in data['features']:
+    properties = feature['properties']
+    row = new_data[properties['id']]
+    properties.update(row)
+
+p.write_text(json.dumps(data, sort_keys=True, indent=4))
